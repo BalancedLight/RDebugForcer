4ba8cb81f31a3cdc6e37f5e54cf1ec9e78aa14e9
JaceDankhover <jacedalhover@gmail.com>
2024-08-09 17:48:52 -0400

    Add project files.

---
+++ RDebugForcer/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using RDLevelEditor;
+using System.Reflection;
+
+namespace RDebugForcer
+{
+    public class RDebugForceMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            var harmony = new HarmonyLib.Harmony("com.lightfall.rdebugforcer");
+            harmony.PatchAll();
+            LoggerInstance.Msg("You're good to go, Intern! I patched up your game to enable some debug functions. Type \"DESPACIT0\" to access level debugging features. Have fun, Intern!");
+            LoggerInstance.Warning("Note: This game was patched which allows developer only functions. Do not report any issues to 7th Beat Games.");
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "scnMenu")
+            {
+                var menu = GameObject.FindObjectOfType<scnMenu>();
+                if (menu != null)
+                {
+                    var field = typeof(scnMenu).GetField("debug_showTestPixels", BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (field != null)
+                    {
+                        field.SetValue(menu, true);
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RDBase), "isDev", MethodType.Getter)]
+    public static class RDBase_isDev_Patch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true; // Force isDev to always return true
+            return false; // Skip the original getter (tm)
+        }
+    }
+
+    [HarmonyPatch(typeof(scnMenu), "Awake")]
+    public static class ScnMenu_Awake_Patch
+    {
+        public static void Prefix(scnMenu __instance)
+        {
+            var field = typeof(scnMenu).GetField("debug_showTestPixels", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field != null)
+            {
+                field.SetValue(__instance, true);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RDLevelEditor.SelectLevelEventPanel), "Awake")]
+    public static class SelectLevelEventPanel_Awake_Patch
+    {
+        private static readonly LevelEventType[] hiddenEvents = new LevelEventType[]
+        {
+            LevelEventType.AdvanceText,
+            LevelEventType.ShowSubdivisionsRows,
+            LevelEventType.CommentShow,
+            LevelEventType.WindowResize,
+            LevelEventType.ReadNarration,
+            LevelEventType.NarrateRowInfo
+        };
+
+        public static void Prefix(RDLevelEditor.SelectLevelEventPanel __instance)
+        {
+            // Filter out hidden events from availableEvents
+            //(idk why but this doesn't even work lmao)
+            var field = typeof(RDLevelEditor.SelectLevelEventPanel).GetField("hiddenEvents", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field != null)
+            {
+                field.SetValue(__instance, hiddenEvents);
+            }
+        }
+    }
+}
---
+++ RDebugForcer/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using MelonLoader;
+using RDebugForcer; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(RDebugForceMod), "Rhythm Doctor Debug Forcer", "0.0.2", "lightfall")]
+[assembly: MelonColor(System.ConsoleColor.DarkYellow)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("7th Beat Games", "Rhythm Doctor")]
